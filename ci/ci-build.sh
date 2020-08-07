9a5e3a8bce7d85625f0ef5a3491b4155d06fdf5b
Yusuf Cemal Celebi <yusuf.celebi@techmac18.local>
2020-08-07 20:26:34 +0300

    Docker image tag is fixed

--- ci/ci-build.sh
+++ ci/ci-build.sh
@@ -4,7 +4,7 @@ set -e
 docker build \
     --build-arg container="$CIRCLE_PROJECT_REPONAME" \
     --build-arg container="$BUILD_NUMBER" \
-    --rm=false --file "./ci/app.dockerfile" -t greetingapp .
+    --rm=false --file "./ci/app.dockerfile" -t yusufcemalcelebi/greetingapp .
 
 
 echo "Build finished"
--- ci/ci-push.sh
+++ ci/ci-push.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 docker login --username yusufcemalcelebi --password $DOCKER_HUB_ACCESS_KEY
 
-docker tag greetingapp greetingapp:$CIRCLE_BUILD_NUM
-docker tag greetingapp greetingapp:latest
+docker tag yusufcemalcelebi/greetingapp yusufcemalcelebi/greetingapp:$CIRCLE_BUILD_NUM
 
 docker push yusufcemalcelebi/greetingapp:$CIRCLE_BUILD_NUM
