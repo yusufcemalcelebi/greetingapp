227c47e2a78aa983008f213a4a8e2437756d39bc
Yusuf Cemal Celebi <yusuf.celebi@techmac18.local>
2020-08-10 23:06:04 +0300

    kubectl is added

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 DECODED_KUBECONFIG=$(echo $ENCODED_KUBECONFIG | base64 -d)
 echo $DECODED_KUBECONFIG > kubeconfig
 export KUBECONFIG=./kubeconfig
@@ -8,5 +12,7 @@ export KUBECONFIG=./kubeconfig
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+kubectl get pods
 
 helm upgrade -i greetingapp ./cd/greetingapp
