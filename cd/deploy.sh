66c20ab1b708fd13328126b2cf813c41afa22f12
Yusuf Cemal Celebi <yusuf.celebi@techmac18.local>
2020-08-10 22:53:06 +0300

    Kubeconfig path is fixed

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-mkdir ~/.kube
 DECODED_KUBECONFIG=$(echo $ENCODED_KUBECONFIG | base64 -d)
-echo $DECODED_KUBECONFIG > ~/.kube/config
+echo $DECODED_KUBECONFIG > kubeconfig
+export KUBECONFIG=./kubeconfig
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
